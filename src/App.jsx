57ee3ab6d3fd829871e0ee41c9933e9042bc9d8c
dileshf <dileshfox@gmail.com>
2026-04-28 03:00:13 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./Header";
+import Footer from "./Footer";
+function App() {
+  return (
+    <div>
+      <Header />
+      <p>Welcome to my website!</p>
+      <Footer />
+    </div>
+  );
+}
+
+export default App
---
+++ src/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+    return (
+        <footer>
+            <p>&copy; 2024 Luca Pizza. All rights reserved.</p>
+        </footer>
+    );
+}
+
+export default Footer;

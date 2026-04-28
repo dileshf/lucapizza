496dd354835197439d0f82d0ed757ab32d6868f8
dileshf <dileshfox@gmail.com>
2026-04-28 22:56:08 +0530

    header upgrade

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import Header from "./Header";
 import Footer from "./Footer";
+import './Header.css';
 function App() {
   return (
     <div>
       <Header />
-      <p>Welcome to my website!</p>
       <Footer />
     </div>
   );
--- src/Header.jsx
+++ src/Header.jsx
@@ -1,16 +1,25 @@
+
 function Header() {
     return (
-        <header>
-            <h1>My website</h1>
-            <nav>
-                <ul>
-                    <li><a href="#">Home</a></li>
-                    <li><a href="#">About</a></li>
-                    <li><a href="#">Contact</a></li>
-                </ul>
-            </nav>
-            <hr></hr>
+        <header className="header">
+            <div className="header-container">
+                <h1 className="header-title">My website</h1>
+                <nav className="nav">
+                    <ul className="nav-list">
+                        <li className="nav-item">
+                            <a href="#" className="nav-link">Home</a>
+                        </li>
+                        <li className="nav-item">
+                            <a href="#" className="nav-link">About</a>
+                        </li>
+                        <li className="nav-item">
+                            <a href="#" className="nav-link">Contact</a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
         </header>
     );
 }
-export default Header
+
+export default Header;
